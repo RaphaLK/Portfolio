be7178103b9d28d246e191e8e3e236b456d98394
RaphaLK <77198909+RaphaLK@users.noreply.github.com>
2024-11-13 21:15:11 -0800

    Added Social Media

--- src/app/Components/AboutMe.js
+++ src/app/Components/AboutMe.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import React from "react";
 import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import { SocialIcon } from "react-social-icons";
 const AboutMe = () => {
   return (
-    <div className="grid grid-rows-2 mt-6 ">
+    <div className="grid grid-rows-3 mt-6 ">
       <div className="grid grid-cols-2 ">
-        <div className="flex items-center">
-          <div className="grid grid-rows-2 items-center">
+        <div className="">
+          <div className="grid grid-rows-2">
             <p className="text-2xl">Hello, I&apos;m Raphael</p>
             <div className="">
               <p className="text-md">
@@ -16,11 +18,24 @@ const AboutMe = () => {
               <div className="flex flex-row items-center text-sm">
                 <p className="border-r px-4 border-gray-300">Pickleball</p>
                 <Separator orientation="vertical" />
-                <p className="border-r px-4 border-gray-300">Ultimate Frisbee</p>
+                <p className="border-r px-4 border-gray-300">
+                  Ultimate Frisbee
+                </p>
                 <Separator orientation="vertical" />
                 <p className="border-r px-4 border-gray-300">Coffee</p>
                 <Separator orientation="vertical" />
                 <p className="px-4 ">Boxing</p>
+              </div>
+            </div>
+            {/* Contact & Social Media */}
+            <div className="mt-8 flex flex-row items-center justify-between">
+              <Button>
+                {" "}
+                <p className="text-sm">Contact Me</p>
+              </Button>
+              <div className="space-x-4">
+                <SocialIcon url="https://github.com/RaphaLK" />
+                <SocialIcon url="https://www.linkedin.com/in/raphaelkusuma/" />
               </div>
             </div>
           </div>
