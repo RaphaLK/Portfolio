d5825d64a61a0c80bc1e85f50cf273155763beb1
RaphaLK <77198909+RaphaLK@users.noreply.github.com>
2024-11-13 20:48:38 -0800

    front page updates

--- src/app/Components/AboutMe.js
+++ src/app/Components/AboutMe.js
@@ -1,20 +1,39 @@
 import Image from "next/image";
 import React from "react";
-
+import { Separator } from "@/components/ui/separator";
 const AboutMe = () => {
   return (
-    <div className="grid grid-rows-2 bg-blue">
-      <div className="grid grid-cols-2">
+    <div className="grid grid-rows-2 mt-6 ">
+      <div className="grid grid-cols-2 ">
         <div className="flex items-center">
-          <p className="text-2xl">Hello, I'm Raphael</p>
+          <div className="grid grid-rows-2 items-center">
+            <p className="text-2xl">Hello, I&apos;m Raphael</p>
+            <div className="">
+              <p className="text-md">
+                Senior Computer Science and Engineering Major at SCU.
+              </p>
+              <Separator className="my-2" />
+              <div className="flex flex-row items-center text-sm">
+                <p className="border-r px-4 border-gray-300">Pickleball</p>
+                <Separator orientation="vertical" />
+                <p className="border-r px-4 border-gray-300">Ultimate Frisbee</p>
+                <Separator orientation="vertical" />
+                <p className="border-r px-4 border-gray-300">Coffee</p>
+                <Separator orientation="vertical" />
+                <p className="px-4 ">Boxing</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <Image
-          src="/Assets/MyPhoto.jpg"
-          width={220}
-          height={200}
-          className="rounded-xl"
-          alt={"My Headshot"}
-        />
+        <div className="flex justify-end">
+          <Image
+            src="/Assets/MyPhoto.jpg"
+            width={220}
+            height={200}
+            className="rounded-xl shadow-2xl"
+            alt={"My Headshot"}
+          />
+        </div>
       </div>
       <div></div>
     </div>
