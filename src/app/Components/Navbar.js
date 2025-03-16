56931bac4392291758cc17afee22686ea64c4440
RaphaLK <77198909+RaphaLK@users.noreply.github.com>
2025-03-15 18:27:01 -0700

    Navbar animations

--- src/app/Components/Navbar.js
+++ src/app/Components/Navbar.js
@@ -8,22 +8,34 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div>
-      <NavigationMenu className="grid grid-cols-3 w-full p-2">
-        <NavigationMenuItem className="flex mx-5 p-2 items-center justify-center border-b border-gray-300">
-          <Link href="/" passHref legacyBehavior>
-            <NavigationMenuLink>About Me</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem className="flex mx-5 p-2 items-center justify-center border-b border-gray-300">
-          <Link href="/Projects" passHref legacyBehavior>
-            <NavigationMenuLink>Projects</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem className="flex mx-5 p-2 items-center justify-center border-b border-gray-300">
-          <Link href="/Work" passHref legacyBehavior>
-            <NavigationMenuLink>Experience</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
+      <NavigationMenu className="grid grid-cols-3 w-full p-3">
+        <div className="transition-all duration-400 ease-in-out text-lg hover:scale-110">
+          <NavigationMenuItem className="flex mx-5 p-2 items-center justify-center border-b border-gray-300 hover:animate-pulse">
+            <Link href="/" passHref legacyBehavior>
+              <NavigationMenuLink>
+                About Me
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </div>
+        <div className="transition-all duration-400 ease-in-out text-lg hover:scale-110">
+          <NavigationMenuItem className="flex mx-5 p-2 items-center justify-center border-b border-gray-300 hover:animate-pulse">
+            <Link href="/Projects" passHref legacyBehavior>
+              <NavigationMenuLink>
+                Projects
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </div>
+        <div className="transition-all duration-400 ease-in-out text-lg hover:scale-110">
+          <NavigationMenuItem className="flex mx-5 p-2 items-center justify-center border-b border-gray-300 hover:animate-pulse">
+            <Link href="/Work" passHref legacyBehavior>
+              <NavigationMenuLink>
+                Experience
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </div>
       </NavigationMenu>
     </div>
   );
